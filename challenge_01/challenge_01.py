0c4a3bf8366447853bb9fb014adcf8987d49f682
Diego Casiano <109401828+diegoocasiano@users.noreply.github.com>
2023-11-04 00:42:12 -0500

    Update challenge_01.py

--- challenge_01/challenge_01.py
+++ challenge_01/challenge_01.py
@@ -1,1 +1,34 @@
+mensaje = """
+    murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco tigre jaguar leopardo oso lobo zorro mapache ardilla elefante rinoceronte hipopotamo cebra jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago flamenco pinguino tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon
+"""
 
+def contar_palabras(mensaje):
+
+    #mensaje.lower() convierte todo el mensaje en minúsculas
+    #split() divide el mensaje en palabras, creando una lista de palabras
+    palabras = mensaje.lower().split()
+
+    #Se crea un diccionario vacío para hacer un seguimiento de cuántas veces aparece cada palabra en el mensaje
+    conteo = {}
+
+    # Recorre las palabras en el mensaje
+    # Palabra es una variable, podría tener cualquier otro nombre
+    for palabra in palabras:
+        if palabra in conteo: #
+            # Si la palabra ya está en el diccionario, incrementa su contador
+            conteo[palabra] += 1
+        else:
+            # Si la palabra no está en el diccionario, agrégala con contador 1
+            conteo[palabra] = 1
+
+    # Crea una lista de palabras únicas junto con su frecuencia (cantidad de veces que aparece en el mensaje)
+    palabras_unicas = [f'{palabra}{conteo[palabra]}' for palabra in conteo]
+
+    # Combina las cadenas de texto en la lista palabras_unicas en una sola cadena de texto (como en el ejemplo de resultado del reto)
+    resultado = ''.join(palabras_unicas)
+
+    return resultado
+
+#Mostrar resultado
+resultado = contar_palabras(mensaje)
+print(resultado)
